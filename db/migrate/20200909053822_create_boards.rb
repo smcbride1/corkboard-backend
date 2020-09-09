9b8fd04014157b22004594703228cf8675d6b69c
Stephen McBride <stevemcbride3@gmail.com>
2020-09-09 11:02:57 -0400

    Updated create boards migration

--- db/migrate/20200909053822_create_boards.rb
+++ db/migrate/20200909053822_create_boards.rb
@@ -1,7 +1,9 @@
 class CreateBoards < ActiveRecord::Migration[6.0]
   def change
     create_table :boards do |t|
-
+      t.string :name
+      t.integer :room_id
+      
       t.timestamps
     end
   end
