0931224d0306dfaf43aebb80ab27206f9bda77ac
Stephen McBride <stevemcbride3@gmail.com>
2020-09-09 11:07:26 -0400

    Updated create notes migration

--- db/migrate/20200909054030_create_notes.rb
+++ db/migrate/20200909054030_create_notes.rb
@@ -1,7 +1,10 @@
 class CreateNotes < ActiveRecord::Migration[6.0]
   def change
     create_table :notes do |t|
-
+      t.string :title
+      t.string :short_content
+      t.string :long_content
+      
       t.timestamps
     end
   end
