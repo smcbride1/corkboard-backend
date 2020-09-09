7f52b18617e3c1c89a5b843258c076d2461802fb
Stephen McBride <stevemcbride3@gmail.com>
2020-09-09 11:03:40 -0400

    Updated create walls migration

--- db/migrate/20200909053904_create_walls.rb
+++ db/migrate/20200909053904_create_walls.rb
@@ -1,7 +1,9 @@
 class CreateWalls < ActiveRecord::Migration[6.0]
   def change
     create_table :walls do |t|
-
+      t.string :name
+      t.integer :room_id
+      
       t.timestamps
     end
   end
