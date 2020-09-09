1832a392703606568883261e686a54319a3fb0f3
Stephen McBride <stevemcbride3@gmail.com>
2020-09-09 11:02:00 -0400

    Updated create users migration

--- db/migrate/20200909053401_create_users.rb
+++ db/migrate/20200909053401_create_users.rb
@@ -1,6 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.integer :facebook_uid
       
       t.timestamps
     end
