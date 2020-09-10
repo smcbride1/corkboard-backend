2ef6a28d5ae307cc76cc6e2e2e2654d449cde6d5
Stephen McBride <stevemcbride3@gmail.com>
2020-09-09 23:58:40 -0400

    Enabled cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+    include ::ActionController::Cookies
+    
+    def authorized?(user_id = nil)
+        if !session.has_key(:user_id) || 
+    end
 end
--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,8 @@ module CorkboardBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    #Use cookies
+    config.middleware.use ActionDispatch::Cookies
   end
 end
