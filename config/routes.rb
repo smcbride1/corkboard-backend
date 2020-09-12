bacc1209b1cbb630b6b0db02b924b57e340fd76b
Stephen McBride <stevemcbride3@gmail.com>
2020-09-12 12:38:24 -0400

    Removed rooms and walls references in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   resources :notes
-  resources :rooms
-  resources :walls
   resources :boards
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
