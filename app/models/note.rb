740724711bfeb03de3155df1a492590f5e2583b1
Stephen McBride <stevemcbride3@gmail.com>
2020-09-18 00:19:34 -0400

    Added default values to note model

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,10 @@
 class Note < ApplicationRecord
+    before_save :default_values
+    def default_values
+        self.title = 'Title' if self.title.nil?
+        self.short_content = 'Short Content' if self.short_content.nil?
+        self.long_content = 'Long Content' if self.long_content.nil?
+        self.x_offset = 0 if self.x_offset.nil?
+        self.y_offset = 0 if self.y_offset.nil?
+    end
 end
