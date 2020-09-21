d300bfc1a70c2116d88285782e1b22cbcf650d72
Stephen McBride <stevemcbride3@gmail.com>
2020-09-21 10:21:20 -0400

    Added dependent destroy to board controller for notes

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,6 @@
 class Board < ApplicationRecord
     before_save :default_values
+    has_many :notes, dependent: :destroy
     def default_values
         self.name = 'New Board' if self.name.nil?
     end
